a28bad98e5364374f620dfbad7a19772a4c7d4e3
retablismo <retablismo@yahoo.es>
2011-06-03 10:25:24 -0400

    Project initialization
    The next issues were accomplished:
    - rails skeleton
    - mysql gem
    - rspec gem
    - database configuration
    - index.html removing
    - gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teaching::Application.config.secret_token = '9f0e2dc882600c21230ed5ba6abfe94dec8081af5eb18130d88c6248404eb5ba2221e00ed013207dd45236b75887c1255fbe3d2ed28452f7eb618b58d62c1552'
